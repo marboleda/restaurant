5e03d49297a1e144675ad8ab939d8a0852a48751
Marco Arboleda <arboleda.marco@gmail.com>
2020-05-28 18:18:17 -0400

    Use modules to import initial page load functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const title = document.createElement('h1');
-const headline = document.createElement('h2');
-const content = document.getElementById('content');
+import initialPageLoad from './initial-page-load';
 
-title.textContent = "The Tropical Grill";
-headline.textContent = "Succulent Filipino Comfort Food";
-
-content.appendChild(title);
-content.appendChild(headline);
+initialPageLoad();
