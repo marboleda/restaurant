7eea84b208889b9f8c66fd4482f701f49b388325
Marco Arboleda <arboleda.marco@gmail.com>
2020-06-01 23:18:39 -0400

    Add fonts and background

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -3,8 +3,10 @@ const initialPageLoad = () => {
     const headline = document.createElement('h2');
     const content = document.getElementById('content');
 
-    title.textContent = "The Tropical Grill";
+    title.textContent = 'The Tropical Grill';
+    title.setAttribute('id', 'restaurant-name')
     headline.textContent = "Succulent Filipino Comfort Food";
+    headline.setAttribute('id', 'headline');
 
     content.appendChild(title);
     content.appendChild(headline);
