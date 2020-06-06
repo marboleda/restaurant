9cee5f137cddc4c7a61b5dae0d0c9526f8ada659
Marco Arboleda <arboleda.marco@gmail.com>
2020-06-05 20:21:13 -0400

    Create e menu functionality

--- src/menu.js
+++ src/menu.js
@@ -1,39 +1,65 @@
 import FriedFishPic from './assets/images/fried-fish.jpg'
+import PorkBBQ from './assets/images/pork-bbq.jpg'
+import FriedRice from './assets/images/fried-rice.jpg'
+import CoconutJuice from './assets/images/coconut-juice.jpg'
+import MangoSmoothie from './assets/images/mango-smoothie.jpg'
+import FreshPapaya from './assets/images/fresh-papaya.jpg'
 
 const displayMenu = () => {
-    const menu = document.createElement('ul');
 
-    for (let i = 0; i < 6; i++) {
-        const menuItem = document.createElement('li');
-        menuItem.setAttribute('data-menu-item', i.toString());
-        menu.appendChild(menuItem);
+    const createMenuLayout = () => {
+        const menu = document.createElement('ul');
+
+        for (let i = 0; i < 6; i++) {
+            const menuItem = document.createElement('li');
+            menuItem.setAttribute('data-menu-item', i.toString());
+            menu.appendChild(menuItem);
+        }
+    
+        document.getElementById('info').appendChild(menu);
     }
 
-    document.getElementById('info').appendChild(menu);
+    const addMenuItem = (menuItemNum) => {
+        const description = document.createElement('h3');
+        const image = document.createElement('img');
+        switch(menuItemNum) {
+            case 0:
+                description.textContent = 'Fried Fish';
+                image.src = FriedFishPic;
+                break;
+            case 1:
+                description.textContent = 'Pork BBQ';
+                image.src = PorkBBQ;
+                break;
+            case 2:
+                description.textContent = 'Fried Rice';
+                image.src = FriedRice;
+                break;
+            case 3:
+                description.textContent = 'Coconut Juice';
+                image.src = CoconutJuice;
+                break;
+            case 4:
+                description.textContent = 'Mango Smoothie';
+                image.src = MangoSmoothie;
+                break;
+            case 5:
+                description.textContent = 'Fresh Papaya';
+                image.src = FreshPapaya;
+                break;
+            default:
+                //nothing
+        }
+        const menuItem = document.querySelector(`[data-menu-item='${menuItemNum}']`)
+        menuItem.appendChild(description);
+        menuItem.appendChild(image);
+    }
 
-    const description0 = document.createElement('h3');
-    description0.textContent = 'Fried Fish';
-    const image0 = document.createElement('img');
-    image0.src = FriedFishPic;
-    const menuItem0 = document.querySelector(`[data-menu-item='0']`);
-    menuItem0.appendChild(description0);
-    menuItem0.appendChild(image0);
-    
+    createMenuLayout();
+    for (let i=0; i < 6; i++) {
+        addMenuItem(i);
+    }
 
-    const description1 = document.createElement('h3');
-    description1.textContent = 'Roast Pork';
-
-    const description2 = document.createElement('h3');
-    description2.textContent = 'Fried Rice';
-
-    const description3 = document.createElement('h3');
-    description3.textContent = 'Fruit Platter';
-
-    const description4 = document.createElement('h3');
-    description4.textContent = 'Guava Juice';
-
-    const description5 = document.createElement('h3');
-    description5.textContent = 'Cheesecake'
 }
 
 export default displayMenu
