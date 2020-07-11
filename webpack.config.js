a7e0f0836b4fc40f769c4bc4835152875e5b5b9f
Marco Arboleda <arboleda.marco@gmail.com>
2020-07-10 23:37:06 -0400

    Optimize images to decrease load time and page size

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,30 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/,
                 use: [
                     'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
                 ],
             },
             {
